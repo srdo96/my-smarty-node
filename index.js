5346396ac9bea218ec0909644419cb9a0c043b48
Sakib Rahman <seu.sakib@gmail.com>
2022-04-26 04:01:43 +0600

    remove comment

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const res = require("express/lib/response");
 const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 5000;
-//add comment
+
 app.use(cors());
 app.use(express.json());
 
